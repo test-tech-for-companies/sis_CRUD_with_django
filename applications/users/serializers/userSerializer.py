1bc4cce8db39e1145ec90add720b14f9696177de
juan-skill <jcurbanom@correo.udistrital.edu.co>
2022-04-20 17:59:00 +0000

    Create user serializer

---
+++ applications/users/serializers/userSerializer.py
@@ -0,0 +1,16 @@
+from applications.users.models.user    import User
+from rest_framework       import serializers
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model  = User
+        fields = ['id', 'passwd', 'name', 'email']
+
+    def to_representation(self, obj):
+        user    = User.objects.get(id=obj.id)
+        return {
+            'id'      : user.id,
+            'name'    : user.name,
+            'email'   : user.email,
+        }
